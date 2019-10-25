9b5a8b8f2922c60523d2819150c2aec0b6fc370b
msamatar0 <mohamedsamatar100@gmail.com>
2019-10-24 20:22:09 -0700

    .

--- objs.py
+++ objs.py
@@ -1,5 +1,6 @@
 import pygame, sys, time, random
 from pygame import *
+from pygame.sprite import *
 
 
 #shorthand for scaling while loading image
@@ -9,9 +10,38 @@ def image_scaler(img, width, height):
     (width, height))
 
 
-#true if everything in the list is the same
-def list_same(li):
-  return all(obj == li[0] for obj in li)
+#true if everything in the list is the same as the given element,
+#in this case a given letter, 'x' or 'o'
+def list_same(li, let):
+  return all(obj == let for obj in li)
+
+
+#true if a single row matches
+def row_match(li, let):
+  rows = [li[0:7:3],
+          li[1:8:3],
+          li[2:9:3],
+          li[0:9:4],
+          li[2:7:2],
+          li[0:3],
+          li[3:6],
+          li[6:9]]
+  #print(*rows, sep = '\n')
+
+  for row in rows:
+    if list_same(row, let):
+      return True
+
+  return False
+
+#row_match([str(x) for x in range(0, 9)], ' ')
+
+
+"""
+    0 1 2
+    3 4 5
+    6 7 8
+"""
 
 
 #contains settings for a game
@@ -42,12 +72,13 @@ class Stats:
 
 
 class Board(Sprite):
-  def __init__(self, config, screen):
+  def __init__(self, config, screen, stats):
     super().__init__()
+    self.config = config
     self.screen = screen
+    self.stats = stats
     self.screen_rect = screen.get_rect()
-    self.image = image_scaler(\
-      'images/ship.png', 200, 200)
+    self.image = image_scaler('images/ship.png', 200, 200)
     self.rect = self.image.get_rect()
     self.grid = [' ']*9
 
@@ -56,20 +87,17 @@ class Board(Sprite):
 
   def check(self, let):
     win = False
-    pass
+    
+    if row_match(self.grid, 'x'):
+      win = True
+      self.stats.x_wins += 1
+
+    elif row_match(self.grid, 'o'):
+      win = True
+      self.stats.o_wins += 1
 
     return win
 
   def blitme(self):
     self.screen.blit(self.image, self.rect)
 
-
-def row_match(li, let):
-  pass
-
-
-"""
-    0 1 2
-    3 4 5
-    6 7 8
-"""

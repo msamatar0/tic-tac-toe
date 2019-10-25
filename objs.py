fcfe77eae46a31b8e42094d07fe4784b484ebef0
msamatar0 <mohamedsamatar100@gmail.com>
2019-10-24 19:55:49 -0700

    .

--- objs.py
+++ objs.py
@@ -1,6 +1,20 @@
 import pygame, sys, time, random
 from pygame import *
 
+
+#shorthand for scaling while loading image
+def image_scaler(img, width, height):
+  return pygame.transform.scale((\
+    pygame.image.load(img)),\
+    (width, height))
+
+
+#true if everything in the list is the same
+def list_same(li):
+  return all(obj == li[0] for obj in li)
+
+
+#contains settings for a game
 class Config:
   def __init__(self):
     self.alt_settings = False
@@ -14,6 +28,8 @@ class Config:
     self.grid_color = (166, 166, 166)
     self.text_color = (255, 255, 255)
 
+
+#contains all stats used by game objs
 class Stats:
   def __init__(self):
     self.active = False
@@ -30,17 +46,30 @@ class Board(Sprite):
     super().__init__()
     self.screen = screen
     self.screen_rect = screen.get_rect()
-    self.image = None
+    self.image = image_scaler(\
+      'images/ship.png', 200, 200)
     self.rect = self.image.get_rect()
     self.grid = [' ']*9
 
   def place(self, pos, let):
     self.grid[pos] = let
 
-  def check(self):
+  def check(self, let):
+    win = False
     pass
+
+    return win
 
   def blitme(self):
     self.screen.blit(self.image, self.rect)
 
-  
+
+def row_match(li, let):
+  pass
+
+
+"""
+    0 1 2
+    3 4 5
+    6 7 8
+"""
